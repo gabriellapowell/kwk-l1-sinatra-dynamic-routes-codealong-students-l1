1ae0cc03696f55a5cf7e42936a24060dbcfaaf69
gabriellapowell <soccerplayer60@icloud.com>
2018-06-14 22:03:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,27 +1,27 @@
-require_relative 'config/environment'
+# require_relative 'config/environment'
 
-class App < Sinatra::Base
+# class App < Sinatra::Base
 
-  # This is a sample static route.
-  get '/hello' do
-    "Hello World!"
-  end
+#   # This is a sample static route.
+#   get '/hello' do
+#     "Hello World!"
+#   end
 
-  # This is a sample dynamic route.
-  get "/hello/:name" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
-  end
+#   # This is a sample dynamic route.
+#   get "/hello/:name" do
+#     @user_name = params[:name]
+#     "Hello #{@user_name}!"
+#   end
 
-get "/goodbye/:name" do 
-  @user_name = params[:name]
-  "goodbye #{@user_name}"
- end 
+# get "/goodbye/:name" do 
+#   @user_name = params[:name]
+#   "goodbye #{@user_name}"
+# end 
  
  
- get "/multiply/:number_one/:number_two" do 
-   x = (params[:number_one].to_i)*(params[:number_two].to_i)
-  "#{x}"
-  end 
+# get "/multiply/:number_one/:number_two" do 
+#   x = (params[:number_one].to_i)*(params[:number_two].to_i)
+#   "#{x}"
+#   end 
 
-end
+# end
